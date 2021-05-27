f5635e4c70589a6c651e2122b3e42a08f241e78d
tobias <twicki12@gmail.com>
2021-05-27 17:43:21 +0200

    use venv instead

--- .jenkins/jenkins.sh
+++ .jenkins/jenkins.sh
@@ -3,9 +3,16 @@
 data_root=/project/s1053/performance/
 
 # activate the environment
-source /project/s1053/install/venv/vcm_1.0/bin/activate
+root_dir=`dirname $0`
+cd ${root_dir}/external/daint_venv && ./install.sh ${root_dir}/venv)
+source ${root_dir}/venv/bin/activate
+# source /project/s1053/install/venv/vcm_1.0/bin/activate
 pip install gprof2dot
 
 
 # generate plots
 python generate_plots.py $data_root
+
+# clean up
+deactivate
+rm -rf venv
